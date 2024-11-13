9096120cac311391b0f9f9288bc7516829830a3e
Keerthik Muruganandam <70237309+krthkmgndm@users.noreply.github.com>
2024-11-13 09:54:28 -0600

    Wrote a small PDB downloader script and got some sample PDB files downloaded.

---
+++ scripts/get_pdbs.py
@@ -0,0 +1,21 @@
+import os
+
+from Bio.PDB.PDBList import PDBList
+
+
+def download_pdb_files(pdb_ids, save_dir="data/raw"):
+    """Download associated PDB files for a given list of PDB IDs"""
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+
+    pdbl = PDBList()
+    for id in pdb_ids:
+        pdbl.retrieve_pdb_file(id, pdir=save_dir, file_format="pdb")
+        print(f"Downloaded: {id}")
+
+
+if __name__ == "__main__":
+    file = open("data/pdb_list.txt", "r")
+    pdb_ids = [id[0:4] for id in file.readlines()]
+    file.close()
+    download_pdb_files(pdb_ids)
