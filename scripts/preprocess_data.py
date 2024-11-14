f2ba7e1755190b574d7c81f04103cbcac3a2e8bd
Keerthik Muruganandam <70237309+krthkmgndm@users.noreply.github.com>
2024-11-14 11:11:47 -0600

    FINALLY ACTUALLY GOT MULTIPLE FILES TO WORK

--- scripts/preprocess_data.py
+++ scripts/preprocess_data.py
@@ -7,12 +7,12 @@ from Bio.PDB.PDBParser import PDBParser
 def calculate_dist_matrix(pdb_file):
 
     parser = PDBParser(QUIET=True)
-    structure = parser.get_structure(pdb_file[-8:-4], pdb_file)
+    structure = parser.get_structure("protein", pdb_file)
 
     alpha_carbons = []
-    for model in structure.get_models():
-        for chain in model.get_chains():
-            for residue in chain.get_residues():
+    for model in structure:
+        for chain in model:
+            for residue in chain:
                 if "CA" in residue:
                     alpha_carbons.append(residue["CA"].coord)
 
@@ -26,11 +26,10 @@ def calculate_dist_matrix(pdb_file):
 
 if __name__ == "__main__":
     path = "./data/raw/"
-    matrices = {
-        file[0:4]: calculate_dist_matrix(os.path.join(path, file))
-        for file in os.listdir(path)
-    }
-
-    for matrix in matrices:
-        print(matrices[matrix])
-        np.savetxt("data/processed/" + matrix + ".csv", matrices[matrix], delimiter=",")
+    files = [os.path.join(path, pdb) for pdb in os.listdir(path)]
+    for file in files:
+        np.savetxt(
+            "data/processed/" + file[-8:-4] + ".csv",
+            calculate_dist_matrix(file),
+            delimiter=",",
+        )
